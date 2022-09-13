1563298ed2abd46311e47bbc67e8ef0baf2b7826
dinhhoanganh0709 <113444600+dinhhoanganh0709@users.noreply.github.com>
2022-09-13 20:24:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: "100000 lí do để yêu ",
+    introTitle: 'Phươnggg yêu dấu của anh',
+    introDesc: `Em có biết là...100000 lí do ấy là quá ít khôngg`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    title: 'Lí do đầu là gì nhỉ??? 🥰',
+    desc: 'Đố em biết',
+    btnYes: 'Muốn biết  <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    question: 'Anh không nói đấyy, cáu chưaaa, vào đây mắng anh đi ',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Em yêu anh',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messDesc: '100000 lí do ấy chung quy lại chỉ 1 lí do, vì người anh yêu nhất là em, chỉ em và mình emmm, yêu em mù quáng',
+    btnAccept: 'Hôn anhhh',
+    messLink: 'https://github.com/zukahai/Confess-Crush' 
 }
